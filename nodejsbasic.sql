12511fd01486b36beb1a6c423838ce43b5d85270
ahihi <bdsthocu20@gmail.com>
2023-08-01 17:39:38 +0700

    #9 http request methods and database

---
+++ nodejsbasic.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 01, 2023 at 10:38 AM
+-- Server version: 8.0.31
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `nodejsbasic`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` tinyint NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `firstName` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `lastName` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `address`, `firstName`, `lastName`) VALUES
+(1, 'email1@gmail.com', 'address1', 'First Name 1', 'Last Name 1'),
+(2, 'email2@gmail.com', 'address2', 'First Name 2', 'Last Name 2');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` tinyint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,7 +1,29 @@
-
+import connection from "../config/connectDB";
 
 let getHomePage = (req, res) => {
-    return res.render('index.ejs');
+    let data = [];
+    connection.query(
+        'SELECT * FROM `users`',
+        function (err, results, fields) {
+            // console.log('check results: ', results);
+            for (let i = 0; i < results.length; i++) {
+                let target = {};
+                Object.assign(target, results[i])
+                data.push(target);
+            }
+            // console.log('check data : ', data);
+            // results.map((row) => {
+            //     data.push({
+            //         id: row.id,
+            //         email: row.email,
+            //         address: row.address,
+            //         firstName: row.firstName,
+            //         lastName: row.lastName
+            //     })
+            // })
+            return res.render('index.ejs', { dataUser: JSON.stringify(data) });
+        }
+    )
 }
 
 module.exports = {
