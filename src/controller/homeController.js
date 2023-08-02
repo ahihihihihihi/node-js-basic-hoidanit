acf9eb3e0de4a29b23e7913fd57a779d22e35bf3
ahihi <bdsthocu20@gmail.com>
2023-08-02 14:56:33 +0700

    #11 ROUTER PARAMS

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,31 +1,17 @@
-import connection from "../config/connectDB";
+import pool from "../config/connectDB";
 
-let getHomePage = (req, res) => {
-    let data = [];
-    connection.query(
-        'SELECT * FROM `users`',
-        function (err, results, fields) {
-            // console.log('check results: ', results);
-            for (let i = 0; i < results.length; i++) {
-                let target = {};
-                Object.assign(target, results[i])
-                data.push(target);
-            }
-            // console.log('check data : ', data);
-            // results.map((row) => {
-            //     data.push({
-            //         id: row.id,
-            //         email: row.email,
-            //         address: row.address,
-            //         firstName: row.firstName,
-            //         lastName: row.lastName
-            //     })
-            // })
-            return res.render('index.ejs', { dataUser: data });
-        }
-    )
+let getHomePage = async (req, res) => {
+    const [rows, fields] = await pool.execute(`select * from users`);
+    return res.render('index.ejs', { dataUser: rows })
+}
+
+let getDetailPage = async (req, res) => {
+    let userId = req.params.id;
+    let [user] = await pool.execute(`select * from users where id = ?`, [userId]);
+    return res.send(JSON.stringify(user));
 }
 
 module.exports = {
-    getHomePage
+    getHomePage,
+    getDetailPage
 }
